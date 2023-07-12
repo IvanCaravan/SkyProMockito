c24d6d46d94c10b6fc84c8162a9f21f83f350116
IvanCaravan <ivan.kirsanov.2000@mail.ru>
2023-07-12 23:01:04 +0300

    initial commit

---
+++ src/main/java/com/example/skypromockito/SkyProMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.skypromockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProMockitoApplication.class, args);
+    }
+
+}
